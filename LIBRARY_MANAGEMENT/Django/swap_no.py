12d9132245e46ba371aec2cb19bbfc80f5019b3a
Ananya Sharma <ananyasharmakpa@gmail.com>
2026-05-21 11:55:12 +0530

    Delete LIBRARY_MANAGEMENT/Django directory

--- LIBRARY_MANAGEMENT/Django/swap_no.py
+++
@@ -1,5 +0,0 @@
-num1=float(input("enter the first number:"))
-num2=float(input("enter the second number:"))
-num1,num2=num2,num1
-print("Before swapping first number is:",num1,"second number is:",num2)
-print("After swapping first number is:",num2,"second number is:",num1)
